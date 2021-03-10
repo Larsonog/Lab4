dd2155771eecabe870286749f115eaceb1645d17
olivia <larsonog@hendrix.edu>
2021-03-10 12:44:08 -0600

    mesh collider added to each step on stairs

---
+++ Assets/Scripts/annotations.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class annotations : MonoBehaviour
+{
+    // stairs https://opengameart.org/content/steps
+    // building texture https://opengameart.org/content/modular-brick-building
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
