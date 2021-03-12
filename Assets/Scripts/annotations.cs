a050e8f26fbe46b7a79c432357465daf7eeeb805
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-03-11 18:57:19 -0600

    annotations added

--- Assets/Scripts/annotations.cs
+++ Assets/Scripts/annotations.cs
@@ -8,6 +8,10 @@ public class annotations : MonoBehaviour
     // building texture https://opengameart.org/content/modular-brick-building
     // pew sound https://freesound.org/people/ani_music/sounds/219619/
     // Concrete wall cc0.link/a/Concrete026 
+    // Boss Texture cc0.link/a/Chip005
+    // Enemy Texture cc0.link/a/Chip003
+    // Marble Texture cc0.link/a/Marble005
+    // Gun Texture https://assetstore.unity.com/packages/3d/props/guns/sci-fi-futuristic-hand-gun-90249
 
     // Start is called before the first frame update
     void Start()
